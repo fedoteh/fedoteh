4a9a8a8ddbb09a3a2beb7714b923ed7c617bf5eb
fedoteh <fedebenavidez@live.com>
2023-03-19 20:32:08 -0300

    hobbies section out, new bg color , email logo in

--- rsc/scripts/main.js
+++ rsc/scripts/main.js
@@ -1,2 +1,23 @@
-const updatedDate = document.getElementById('date');
-updatedDate.innerHTML= new Date().getFullYear();
+function handleHeaderOnScroll() {
+    const header = document.querySelector("header");
+    let prevScrollPos = window.scrollY;
+    let currentScrollPos = window.scrollY;
+    window.onscroll = () => {
+      currentScrollPos = window.scrollY;
+      if (prevScrollPos > currentScrollPos) {
+        header.style.top = "0";
+      } else {
+        header.style.top = "-60px";
+      }
+      prevScrollPos = currentScrollPos;
+    }
+    
+}
+
+function showCurrentYear() {
+    const yearElement = document.getElementById('year');
+    yearElement.innerHTML= new Date().getFullYear();
+}
+
+handleHeaderOnScroll();
+showCurrentYear();
